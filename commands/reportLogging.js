ccb456e342a36417d80fd62318d109093b461789
Darius Wilson <dariuswilsonxu@gmail.com>
2023-02-15 23:28:29 -0600

    Channel Report Logging

---
+++ commands/reportLogging.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('log')
+        .setDescription('Sets the channel where report logs are.')
+        .addChannelOption(option => option.setName('channel').setDescription('(input log channel)')
+        .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+	async execute(interaction) {
+		await interaction.reply({
+
+		});
+	},
+};
